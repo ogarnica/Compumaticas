0d9157b267e37b9aa3f9a87eb07d1425b68325e1
Oscar Garnica <ogarnica@ucm.es>
2016-10-23 18:25:45 +0200

    Version finalizada del ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -1,11 +1,22 @@
-print('JUGADOR 1')
-palabra = input('DI UNA PALABRA')
-print('JUGADOR 2')
-print('tiene %s letras' % len(palabra))
+guesses = ''
+palabra = 'computadora'
+vidas = 0
+while vidas < 15:
 
-for letra in palabra:
-    letra = input('di una letra')
-    if letra == 'palabra':
-        print(letra)
-    else:
-        print('-')
+    fail = 0
+    for caracter in palabra:
+        if caracter in guesses:
+            print(caracter)
+        else:
+            print('_')
+            fail += 1
+    if fail == 0:
+        print('has ganado')
+        break
+    guess = input('di la letra')
+    guesses += guess
+    if guess not in palabra:
+        vidas -= 1
+        print('error')
+        if vidas == 0:
+            print('GAME OVER   xDxDxD...')

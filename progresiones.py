02df2fab6a1885a89a93ee81589d931ff9da6ee7
Oscar <ogarnica@ucm.es>
2017-06-16 16:03:27 +0200

    Correcciones ortograficas

--- progresiones.py
+++ progresiones.py
@@ -1,6 +1,6 @@
 a1 = int(input('primer numero: '))
-d = int(input('desplacamiento: '))
-n = int(input('ultimo numero: '))
+d = int(input('desplazamiento: '))
+n = int(input('ultimo termino: '))
 an = a1+(n-1)*d
 suma = (a1+an)/2*n
 print(suma)
